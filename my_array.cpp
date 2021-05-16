6c1575a14b0d95ec38ebcbf8eb1a52747028aef3
hyoomi <hyoo_mi@naver.com>
2021-05-16 22:55:15 +0900

    Initialize my_array

---
+++ my_array.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <algorithm>
+#include <iostream>
+
+template <typename T>
+class my_array{
+    T* data;
+    size_t n;
+public:
+    my_array(int n){
+        this->n = n;
+        data = new T[n];
+    }
+    my_array(const my_array<T>& copy){
+        n = copy.n;
+        data = new T[n];
+        for(int i = 0; i < n; i++)
+            data[i] = copy.data[i]; //data[i] = copy[i];
+    }
+    T& operator[](int index){
+        return data[index];
+    }
+    T& at(int index){
+        if(index < n)
+            return data[index];
+        throw "index out of range";
+    }
+    size_t size() const{
+        return n;
+    }
+    ~my_array(){
+        delete[] data;
+    }
+
+}
