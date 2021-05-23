c5edbc9b3bb5a2ea2e7a95d5b343581ccd23cac0
hyoomi <hyoo_mi@naver.com>
2021-05-23 23:26:40 +0900

    Add push and pop

--- my_list.cpp
+++ my_list.cpp
@@ -5,3 +5,24 @@ struct my_list_node{
     int data;
     my_list_node* next;
 }
+
+class my_list{
+public:
+    using node = my_list_node;
+private:
+    node* head;
+public:
+    void push_front(int val){
+        auto new_node = new node {val, NULL};
+        if(head != NULL)
+            new_node->next = head;
+        head = new_node;
+    }
+    void pop_front(){
+        auto first = head;
+        if(head){
+            head = head->next;
+            delete first;
+        }
+    }
+}
